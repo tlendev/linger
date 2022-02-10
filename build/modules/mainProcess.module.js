927758860d4b45b139c8e6e413b2f3c9bc164244
tlendev <oxyaego98@gmail.com>
2022-02-10 17:32:34 +0100

    Cleanup part 2

--- build/modules/mainProcess.module.js
+++ build/modules/mainProcess.module.js
@@ -1,21 +1,22 @@
 import { getLottieQuestion, solveLottie } from './resolver.module.js';
+const getProgress = async (page)=>{
+    return await page.$eval('body > div.container-top > div.text-center > div > h5', (node)=>{
+        return parseInt(node.textContent.trim().match(/(.+)\/20/)[1]); // @returns XX/20
+    });
+};
 const mainProcess = async (page)=>{
     await page.goto('https://lingos.pl/students/learning', {
         waitUntil: 'networkidle0'
     });
-    const startingProgress = +await page.$eval('body > div.container-top > div.text-center > div > h5', (node)=>{
-        return node.innerHTML.trim().match(/(.+)\/20/)[1]; // @returns XX/20
-    });
+    const startingProgress = await getProgress(page);
     let progress = startingProgress;
     while(progress < 20){
-        const q = await getLottieQuestion(page);
-        await solveLottie(page, q);
+        const qusetion = await getLottieQuestion(page);
+        await solveLottie(page, qusetion);
         if (progress === 19) {
             return;
         }
-        progress = +await page.$eval('body > div.container-top > div.text-center > div > h5', (node)=>{
-            return node.innerHTML.trim().match(/(.+)\/20/)[1]; // @returns XX/20
-        });
+        progress = await getProgress(page);
     }
 };
 export { mainProcess };
--- src/modules/mainProcess.module.ts
+++ src/modules/mainProcess.module.ts
@@ -1,31 +1,29 @@
 import { Page } from 'puppeteer';
 import { getLottieQuestion, solveLottie } from './resolver.module.js';
 
+const getProgress = async (page: Page): Promise<number> => {
+	return await page.$eval(
+		'body > div.container-top > div.text-center > div > h5',
+		(node) => {
+			return parseInt(node.textContent.trim().match(/(.+)\/20/)[1]); // @returns XX/20
+		}
+	);
+};
+
 const mainProcess = async (page: Page) => {
 	await page.goto('https://lingos.pl/students/learning', {
 		waitUntil: 'networkidle0',
 	});
-
-	const startingProgress: number = +(await page.$eval(
-		'body > div.container-top > div.text-center > div > h5',
-		(node) => {
-			return node.innerHTML.trim().match(/(.+)\/20/)[1]; // @returns XX/20
-		}
-	));
+	const startingProgress = await getProgress(page);
 	let progress = startingProgress;
 
 	while (progress < 20) {
-		const q = await getLottieQuestion(page);
-		await solveLottie(page, q);
+		const qusetion = await getLottieQuestion(page);
+		await solveLottie(page, qusetion);
 		if (progress === 19) {
 			return;
 		}
-		progress = +(await page.$eval(
-			'body > div.container-top > div.text-center > div > h5',
-			(node) => {
-				return node.innerHTML.trim().match(/(.+)\/20/)[1]; // @returns XX/20
-			}
-		));
+		progress = await getProgress(page);
 	}
 };
 
